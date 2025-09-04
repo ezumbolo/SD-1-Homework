a4729f31422e20bcead005c9f75e05a11a73cbe4
ezumbolo <evan.j.zumbolo@gmail.com>
2025-09-04 13:34:51 -0400

    Update main.java

--- Assignment 1/main.java
+++ Assignment 1/main.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Debug {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How old are you?");
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old!");
+        int num1 = 10;
+        int num2 = 37;
+        System.out.println(num1 + num2);
+        //The main thing I learned in this assignment is how to import a scanner. I also learned how to use the scanner to ask a user questions and print back something based on the user's response!
+    }
+}
 
